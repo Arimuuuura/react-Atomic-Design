e5133dc9caf76f92191e0882162bdb7d63fc8f39
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-08 16:45:52 +0900

    secondary button component 作成

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 
 export const App = () => {
   return (
     <>
       <PrimaryButton>test</PrimaryButton>
+      <SecondaryButton>second</SecondaryButton>
     </>
   );
 }

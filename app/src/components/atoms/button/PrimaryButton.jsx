7bca0b075ab1f3fb1759f7db8d7f7ace8f8588e9
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-08 16:54:14 +0900

    共通部分を BaseButton component にまとめた

--- app/src/components/atoms/button/PrimaryButton.jsx
+++ app/src/components/atoms/button/PrimaryButton.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { BaseButton } from './BaseButton'
 
-const SButton = styled.button`
+const SButton = styled(BaseButton)`
   background-color: #40514e;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  border-radius: 100px;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
 `
 
 export  const PrimaryButton = (props) => {

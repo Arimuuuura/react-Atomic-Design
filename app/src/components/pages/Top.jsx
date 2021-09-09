cf23f5a2ce4d92eadf4245958e108dce6c1648b2
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-09 23:20:44 +0900

    pages 雛形作成

---
+++ app/src/components/pages/Top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const SContainer = styled.div`
+  text-align: center;
+`
+
+export const Top = () => {
+  return (
+    <SContainer>
+      <h2>Topページ</h2>
+    </SContainer>
+  )
+}
---
+++ app/src/components/pages/Users.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const SContainer = styled.div`
+  text-align: center;
+`
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Users一覧</h2>
+    </SContainer>
+  )
+}

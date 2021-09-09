b23b5aae6884c61108677cb12c8c8b5c961c292a
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-09 23:55:44 +0900

    page ごとの出し分け

--- app/src/router/Router.jsx
+++ app/src/router/Router.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Top } from '../components/pages/Top'
 import { Users } from '../components/pages/Users'
+import { DefaultLayout } from '../components/templates/DefaultLayout'
+import { HeaderOnly } from '../components/templates/HeaderOnly'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Top />
+          <DefaultLayout>
+            <Top />
+          </DefaultLayout>
         </Route>
         <Route exact path="/users">
-          <Users />
+          <HeaderOnly>
+            <Users />
+          </HeaderOnly>
         </Route>
       </Switch>
     </BrowserRouter>

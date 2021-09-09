f405cb59f7bc13cf22301d42c1d489fe316edbd9
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-09 23:55:57 +0900

    レイアウト修正

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,5 @@
 import { Router } from "./router/Router";
 
-const user = {
-  name: 'arimura',
-  image: 'https://source.unsplash.com/7AIDE8PrvA0',
-  email: 'abc@abc.com',
-  tell: '080-1234-1234',
-  company: {
-    name: 'aaa株式会社',
-  },
-  website: 'https://google.com',
-}
-
 export const App = () => {
   return (
     <Router />
